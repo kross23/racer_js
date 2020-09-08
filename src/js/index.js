89209e6dc11f243e63e70a75606fd7312d7bba42
kross <662242@mail.ru>
2020-09-08 17:46:38 +0500

    lesson1

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,47 @@
 // Main js file
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+'use strict';
+const score = document.querySelector('.score'),
+	start = document.querySelector('.start'),
+	gameArea = document.querySelector('.gameArea'),
+	car = document.createElement('div');
+	car.classList.add('car');
+const keys = {
+	ArrowUp: false,
+	ArrowDown: false,
+	ArrowRigth: false,
+	ArrowLeft: false,
+};
+const setting = {
+	start: false,
+	score: 0,
+	speed: 3,
 
-console.log("its work");
+};
+const playGame = () => {
+	console.log('play');
+	if(setting.start === true){
+		requestAnimationFrame(playGame);
+	}
+	
+};
+
+const startGame = () => {
+	start.classList.add('hide');
+	setting.start = true;
+	gameArea.appendChild(car);
+	requestAnimationFrame(playGame);
+};
+const startRun = event => {
+	event.preventDefault();
+	keys[event.key] = true;
+	console.log(keys);
+};
+const stopRun = () => {
+	event.preventDefault();
+	keys[event.key] = false;
+	console.log(keys);
+};
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
